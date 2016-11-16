296ddd92b5cf6de20457d9f98434d272cab575ce
Markuless64 <marcus.groll@stud-mail.uni-wuerzburg.de>
2016-11-16 20:14:25 +0100

    Update H_Work_16_11.R

--- H_Work_16_11.R
+++ H_Work_16_11.R
@@ -2,19 +2,33 @@ install.packages("raster")
 
 z <- runif(100, min=0, max = 1)
 
-b <- raster(nrows=10, ncols=10, vals=z)
+zz <- raster(nrows=10, ncols=10, vals=z)
 
-plot(b)
-z <- data.frame(z)
+plot(zz)
 
-seg1 <- z[,1]<0.251
-seg1 <- raster(vals=seg1)
+seg1 <- z[z<0.25]
+seg1 <- t(seg1)
+seg2 <- z[z>0.25 & z<0.5]
+seg2 <- t(seg2)
+seg3 <- z[z>0.5 & z<0.75]
+seg3 <- t(seg3)
+seg4 <- z[z>0.75]
+seg4 <- t(seg4)
 
-table(z$z)
-
-b <- quantile(b, probs = seq(0,1, 0.25))
-aa <- raster(quantile(z, probs = seq(0,1, 0.25)))
-
+a <- matrix(seg1, nrow = 3)
+a <- raster(a)
 plot(a)
+b <- matrix(seg2, nrow = 3)
+b <- raster(b)
+plot(b)
+c <- matrix(seg3, nrow = 3)
+c <- raster(c)
+plot(c)
+d <- matrix(seg4, nrow = 3)
+d <- raster(d)
+plot(d)
 
-a <- data.frame(a)
+
+stack(a,b,c,d)
+plot(a)
+plot(b, add=TRUE)
