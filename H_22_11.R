3e733c35d4c1fbd21450cd392e64af9e5b043716
Markuless64 <marcus.groll@stud-mail.uni-wuerzburg.de>
2016-11-21 15:13:01 +0100

    Change
    Change for 21_11

--- H_22_11.R
+++ H_22_11.R
@@ -5,9 +5,8 @@ z <- runif(100, min=-1, max = 1)
 zz <- raster(nrows=10, ncols=10, vals=z)
 
 zz[zz<0.25] <- 1
-zz[zz>0.25 & z<0.5] <- 2
-zz[zz>0.5 & z<0.75] <- 3
-zz[zz>0.75 & z<1.0] <- 4
+zz[zz>0.25 & zz<0.5] <- 2
+zz[zz>0.5 & zz<0.75] <- 3
+zz[zz>0.75 & zz<1.0] <- 4
 
 plot(zz)
-plot(z)
