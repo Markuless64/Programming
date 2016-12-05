d53a02a0c2966fa5ea20d822ccb97a7eb235816e
Markuless64 <marcus.groll@stud-mail.uni-wuerzburg.de>
2016-12-05 22:54:24 +0100

    Homework_06_12

--- 30_11.R
+++ 30_11.R
@@ -12,21 +12,15 @@ install.packages("randomForest")
 library(randomForest)
 
 allbands  <- brick("E:/Uni/EAGLE/MB1/01_Data/02_Raster/RapidEye_Ammersee/re20110906_ortho_ls_prj_grto0509_sub.tif")
-allbands <- raster("E:/Uni/EAGLE/MB1/01_Data/02_Raster/RapidEye_Ammersee/re20110906_ortho_ls_prj_grto0509_sub.tif")
 
-a <- readOGR("E:/Uni/EAGLE/Program/Qgis","Water1")
-b <- readOGR("E:/Uni/EAGLE/Program/Qgis","Meadow1")
-c <- readOGR("E:/Uni/EAGLE/Program/Qgis","Urban1")
-d <- readOGR("E:/Uni/EAGLE/Program/Qgis","Forest1")
+a <- readOGR("E:/Uni","All")
 
-bb <- superClass()
 aa <- superClass (allbands, trainData = a,
-                  nSamples = 1000, responseCol = "Water", 
-                  predType = "raw", kfold = 5, tuneLength = 3,
-                  overwrite = TRUE)
+                  nSamples = 1000, responseCol = "Id", 
+                  predType = "raw")
+
 map <- predict(aa,allbands)
 x11()
 plotRGB (allbands, 3,2,1, stretch ="lin")
 x11()
-plot(map)
-
+plot
---
+++ H_06_12.R
@@ -0,0 +1,29 @@
+install.packages("RStoolbox")
+library(RStoolbox)
+install.packages("raster")
+library(raster)
+install.packages("cluster")
+library(cluster)
+install.packages("rgdal")
+library(rgdal)
+install.packages("e1071")
+library(e1071)
+install.packages("randomForest")
+library(randomForest)
+
+allbands  <- brick("F:/Uni/EAGLE/MB1/01_Data/02_Raster/RapidEye_Ammersee/re20110906_ortho_ls_prj_grto0509_sub.tif")
+
+a <- readOGR("F:/Uni","All")
+
+aa <- superClass (allbands, trainData = a,
+                  nSamples = 1000, responseCol = "Name", 
+                  predType = "raw")
+
+map <- predict(aa,allbands)
+x11()
+plot(map)
+map_new <- ggR(map, forceCat = TRUE, geom_raster = TRUE)+
+  scale_fill_manual(values = c("green", "brown", "red", "blue"),
+                    labels = c("Forest", "Meadow" , "Urban", "Water"))
+
+map_new
